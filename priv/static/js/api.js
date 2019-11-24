f09f67e3f3fbc0b51e8db728c04e35a25372a4f3
Rouni Yin <yinrouni@github.com>
2019-11-24 16:38:26 -0500

    add map

--- priv/static/js/api.js
+++ priv/static/js/api.js
@@ -9,7 +9,6 @@ function getLatitude(){
 	return latitude;
 }
 
-
 document.body.onload = function() {showPosition()};
 function showPosition() {
         if(navigator.geolocation) {
@@ -17,9 +16,12 @@ function showPosition() {
                 var positionInfo = "Your current position is (" + "Latitude: " + position.coords.latitude + ", " + "Longitude: " + position.coords.longitude + ")";	
 		document.getElementById('latitude').value=position.coords.latitude;
 		document.getElementById('longitude').value=position.coords.longitude;
-		document.forms[0].submit();
                 document.getElementById("result").innerHTML = positionInfo;
-		    
+		var latlon = position.coords.latitude + "," + position.coords.longitude;
+
+  var img_url = "https://maps.googleapis.com/maps/api/staticmap?center="+latlon+"&zoom=15&size=600x300&sensor=false&key=AIzaSyBD687lI9Zy6weGMG3JV4F0tjPd63QxfyU";
+
+  document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";
             });
         } else {
             alert("Sorry, your browser does not support HTML5 geolocation.");
