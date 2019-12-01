2aa252158c0481a44ad4b72055841bed589d81a3
Rouni Yin <yinrouni@github.com>
2019-12-01 18:23:31 -0500

    password & profile link

--- lib/popo/users/user.ex
+++ lib/popo/users/user.ex
@@ -7,7 +7,8 @@ defmodule Popo.Users.User do
     field :latitude, :float
     field :longitude, :float
     field :name, :string
-    field :password, :string
+    field :password_hash, :string
+    field :password, :string, virtual: true
     field :password_confirmation, :string, virtual: true
     has_one :profile, Popo.Profiles.Profile
     has_many :posts, Popo.Posts.Post
@@ -24,6 +25,16 @@ defmodule Popo.Users.User do
     |> cast(attrs, [:email, :name, :password, :longitude, :latitude, :password_confirmation])
     |> validate_confirmation(:password)
     |> validate_length(:password, min: 12)
-    |> validate_required([:email, :name, :password])
+    |> hash_password()
+    |> validate_required([:email, :name, :password_hash])
+  end
+
+    def hash_password(cset) do
+    pw = get_change(cset, :password)
+    if pw do
+      change(cset, Argon2.add_hash(pw))
+    else
+      cset
+    end
   end
 end
